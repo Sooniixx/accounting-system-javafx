a51d5e75b533b6105b9b5b22860a6c00cbc94dd2
Sofia Fesenko <sofiafesenko1@gmail.com>
2026-05-24 19:53:27 +0300

    Initialize Maven project structure and resources

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS employees (
+                                         id INT AUTO_INCREMENT PRIMARY KEY,
+                                         full_name VARCHAR(255) NOT NULL,
+    position VARCHAR(255) NOT NULL,
+    salary DOUBLE NOT NULL,
+    department VARCHAR(255) NOT NULL
+    );
