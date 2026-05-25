0b9204d209aca21d41d84e4c074abbbe7236913b
Diana-Yakovlieva <diyakovlieva1@gmail.com>
2026-05-25 17:05:50 +0300

    Implemented full CRUD and search for Employee

---
+++ src/main/java/service/EmployeeService.java
@@ -0,0 +1,84 @@
+package service;
+
+import db.DatabaseManager;
+import model.Employee;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeService {
+    // Create (insert)
+    public void addEmployee(Employee e) {
+
+        String sql = "INSERT INTO employees (full_name, position, salary, department) VALUES (?, ?, ?, ?)";
+
+        try (Connection conn = DatabaseManager.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, e.getFullName());
+            ps.setString(2, e.getPosition());
+            ps.setDouble(3, e.getSalary());
+            ps.setString(4, e.getDepartment());
+
+            ps.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    // Read (select all)
+    public List<Employee> getAllEmployees() {
+
+        List<Employee> list = new ArrayList<>();
+
+        String sql = "SELECT * FROM employees";
+
+        try (Connection conn = DatabaseManager.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+
+                Employee e = new Employee();
+
+                e.setId(rs.getInt("id"));
+                e.setFullName(rs.getString("full_name"));
+                e.setPosition(rs.getString("position"));
+                e.setSalary(rs.getDouble("salary"));
+                e.setDepartment(rs.getString("department"));
+
+                list.add(e);
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return list;
+    }
+
+    // Update
+    public void updateEmployee(Employee e) {
+
+        String sql = "UPDATE employees "
+                + "SET full_name=?, position=?, salary=?, department=? "
+                + "WHERE id=?";
+
+        try (Connection conn = DatabaseManager.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, e.getFullName());
+            ps.setString(2, e.getPosition());
+            ps.setDouble(3, e.getSalary());
+            ps.setString(4, e.getDepartment());
+            ps.setInt(5, e.getId());
+
+            ps.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
