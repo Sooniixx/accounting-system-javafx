c39840b125ec9d258d588b520787b9fe9001a559
Diana-Yakovlieva <diyakovlieva1@gmail.com>
2026-05-25 16:52:45 +0300

    Completed Employee model with constructors and getters/setters

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -6,4 +6,62 @@ public class Employee {
     private String position;
     private double salary;
     private String department;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String fullName, String position, double salary, String department) {
+        this.id = id;
+        this.fullName = fullName;
+        this.position = position;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public Employee(String fullName, String position, double salary, String department) {
+        this.fullName = fullName;
+        this.position = position;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
