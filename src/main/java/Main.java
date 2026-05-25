9723c433c6a6dda390f6241141440e067b509687
Diana-Yakovlieva <diyakovlieva1@gmail.com>
2026-05-25 18:53:20 +0300

    Added application entry point and initialized JavaFX launch for frontend integration

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import view.MainApp;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starting Employee Management System...");
+        javafx.application.Application.launch(MainApp.class, args);
+    }
+}
