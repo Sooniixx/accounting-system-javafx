b90716f55c158ccac66055f13fd057eeb322cad2
Diana-Yakovlieva <diyakovlieva1@gmail.com>
2026-05-25 16:26:35 +0300

    Created Employee model and project structure

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Employee {
+    private int id;
+    private String fullName;
+    private String position;
+    private double salary;
+    private String department;
+}
